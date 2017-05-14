0ab5d93367cdbe52722533584093a0778038b22e
Maxime Nory <maximenory@yahoo.fr>
2017-05-14 17:45:56 +0200

    Details content
    Added details content.

--- movies/src/details.js
+++ movies/src/details.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './details.css';
 import {getTitle} from './utils.js';
+import moment from 'moment';
 
 class Details extends Component{
 
@@ -20,7 +21,11 @@ class Details extends Component{
       );
     else{
 
-      var name = getTitle(this.props.data);
+      var name = getTitle(this.props.data),
+      backdrop = "http://image.tmdb.org/t/p/w1280/" + this.props.data.backdrop_path,
+      backAlt = name + " backdrop image",
+      date = moment(this.props.data.release_date).format('MMM DD YYYY'),
+      overview = this.props.data.overview;
 
       return (
         <div
@@ -28,7 +33,16 @@ class Details extends Component{
           style={{marginLeft: 0}}
           >
             <div className='close-button' onClick={this.props.exit}/>
-            <h1 className='mtitle'>Details about {name}</h1>
+            <img src={backdrop}
+                 alt={backAlt}
+                 className='backdrop'
+            />
+            <div className='movie-details'>
+              <h1 className='mtitle'>{name}<br/></h1>
+              <h4 className='date'>{date}</h4>
+
+              <p className='overview'>{overview}</p>
+            </div>
         </div>
       );
     }
