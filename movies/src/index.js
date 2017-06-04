455630cd5c391a65eb0ab3721c50d605dbb924d7
Maxime Nory <maximenory@yahoo.fr>
2017-06-04 22:31:41 +0200

    Added react-router
    Added react-router to set customs URLs for the app.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router'
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render((
+
+  <BrowserRouter>
+      <Switch>
+        <Route path='/' component={App}/>
+      </Switch>
+  </BrowserRouter>
+),
   document.getElementById('root')
 );
